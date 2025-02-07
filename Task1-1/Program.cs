950ca5b8d369c634a0cbfb21af48c696d098a246
tatii1111 <tanuta1111@mail.ru>
2025-02-07 13:44:44 +0300

    new

--- Task1-1/Program.cs
+++ Task1-1/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+                Console.Write("a=");
+                int a = Console.Read();
+                Console.WriteLine();
+                Console.Write("b=");
+                int b = Console.Read();
+                Console.WriteLine();
+                Console.Write("c=");
+                int c = Console.Read();
+                Console.WriteLine();
+                Console.WriteLine(a);
+                Console.WriteLine("a=" + (a=c) + " b=" + (b=a) + " c=" + (c=b));
+            }
         }
     }
-}
